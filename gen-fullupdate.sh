9edfb20e69be8fc0d95abaaca533ab63cdaf6491
penn5 <penn5@users.noreply.github.com>
2018-11-11 16:02:30 +0000

    Update gen-fullupdate.sh
    add metadata stuff

--- gen-fullupdate.sh
+++ gen-fullupdate.sh
@@ -3,5 +3,6 @@ mkdir ota
 cp -R system ota/system
 cd ota
 echo -n incr > type
+ls system > add.files
 tar -cvjpf update.tar.bz2 --strip-components=1 --transform 's/.\///' --show-transformed-names .
 echo OTA Generated at ota/update.tar.bz2
