c5c593fd7125aa3ef6f00299b643860c3e618755
Hackintosh5 <penn.mackintosh@gmail.com>
2018-11-19 11:47:15 +0000

    stash

--- host/gen-incrupdate.sh
+++ host/gen-incrupdate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-#  Copyright (C) 2018 Mohamed Nahil Hamdhy
+#  Copyright (C) 2018 Mohamed Nahil Hamdhy and Penn Mackintosh
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -18,13 +18,13 @@
 
 dif(){
 	common(){
-		[[ $(cat $tmp/common | grep "$1") ]] || echo $1 >> $tmp/common
+		[[ $(cat $tmp/common | grep "$1") ]] || (echo $1 >> $tmp/common;echo update: $i)
 	}
 	cat $1 | while read -r i; do 
-		[[ $(cat $2 | grep "$i") ]] && common $i || echo $i >> $tmp/del
+		[[ $(cat $2 | grep "$i") ]] && common $i || (echo $i >> $tmp/ota/rm.files;echo rm: $i)
 	done
 	cat $2 | while read -r i; do
-		[[ $(cat $1 | grep "$i") ]] && common $i || echo $i >> $tmp/add
+		[[ $(cat $1 | grep "$i") ]] && common $i || (echo $i >> $tmp/ota/add.files;echo add: $i)
 	done
 }
 
@@ -46,12 +46,25 @@ list(){
 		dif $tmp/old $tmp/new
 	done
 	for m in $(cat $tmp/common); do
-		[[ "$(md5 $old/$m)" = "$(md5 $new/$m)" ]] || echo $n >> $tmp/upd
+		if ! [[ -f "$old/$m" ]]; then
+			echo $m >> $tmp/ota/add.files
+		fi
+		if ! [[ -f "$new/$m" ]]; then
+			echo $m >> $tmp/ota/rm.files
+		fi
+		[[ "$(md5 $old/$m)" = "$(md5 $new/$m)" ]] || echo $m >> $tmp/upd
+	done
+	cd $new
+	find . -type d | tar --no-recursion -cpT - -f - | (cd $tmp/ota/system;tar -xpf -)
+	for m in $(cat $tmp/upd); do
+		echo $m >> $tmp/ota/update.files
+		cp -aR $new/$m $tmp/ota/system/$m
 	done
 
 }
-mkdir tmp
 tmp="$PWD/tmp"
+mkdir -p $tmp/ota/system
 old=$(realpath "$1")
 new=$(realpath "$2")
 list
+
